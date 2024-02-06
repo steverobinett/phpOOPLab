924dee283a3ee81dfd59c4f5836964b3e7a11e58
Steve Robinett <steven.robinett@gfcmsu.edu>
2024-02-05 18:46:11 -0700

    formatting on bank

--- bank.php
+++ bank.php
@@ -1,64 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="layout.css">
     <title>Document</title>
 </head>
+
 <body>
     <h3>OOP Lab</h3>
-      
+
     <?php
-        class Account {
-            private $acctNum;
-            private $balance;
+    class Account
+    {
+        private $acctNum;
+        private $balance;
 
-            function __construct($num, $bal){
-                $this->acctNum = $num;
-                $this->balance = $bal;
-            }
+        function __construct($num, $bal)
+        {
+            $this->acctNum = $num;
+            $this->balance = $bal;
+        }
 
-            function getAcctNum(){
-                return $this->acctNum;
-            }
+        function getAcctNum()
+        {
+            return $this->acctNum;
+        }
 
-            function getBalance(){
-                return $this->balance;
-            }
+        function getBalance()
+        {
+            return $this->balance;
+        }
 
-            function deposit($amt){
-                $this->balance += $amt;
-            }
+        function deposit($amt)
+        {
+            $this->balance += $amt;
+        }
 
-            
-        function withdrawl($amt){
-            if ($this->balance - $amt < 0){
+
+        function withdrawl($amt)
+        {
+            if ($this->balance - $amt < 0) {
                 echo "Nope. Not enough<br/>";
-            }
-            else{
+            } else {
                 $this->balance -= $amt;
             }
         }
-        }
-
-        $a = new Account("SR100", 100.0);
-        echo $a->getAcctNum()."<br />";
-        echo $a->getBalance()."<br />";
-
-        
-        $a->deposit(50.0);
-        echo $a->getBalance()."<br />";
-
-        $a->withdrawl(1000.0);
-        echo $a->getBalance()."<br />";
-
-        // $a->balance = 0;
-        echo $a->getBalance()."<br />";
-
+    }
     ?>
+    <div class="container">
+
+        <div class="column">
+            <?php
+
+            $a = new Account("SR-100", 100.0);
+            echo $a->getAcctNum() . "<br />";
+            echo $a->getBalance() . "<br />";
+
+
+            $a->deposit(50.0);
+            echo $a->getBalance() . "<br />";
+
+            $a->withdrawl(1000.0);
+            echo $a->getBalance() . "<br />";
+
+            // $a->balance = 0;
+            echo $a->getBalance() . "<br />";
+
+            ?>
+        </div>
+    </div>
 
 
 </body>
+
 </html>
